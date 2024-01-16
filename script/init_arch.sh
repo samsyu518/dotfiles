7d73f37142dab2b5794090b845428f360f9ec6ae
Sam Syu <samsyu518@gmail.com>
2024-01-17 00:46:30 +0800

    arch init add python and en_US.UTF-8 locale

--- script/init_arch.sh
+++ script/init_arch.sh
@@ -12,11 +12,12 @@ pacman -Su --noconfirm
 #basic build and develop tools
 pacman -S --noconfirm reflector
 reflector --country "Japan,South Korea,Taiwan,United States" --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
-pacman -S --noconfirm base base-devel git vim neovim tmux wget zsh openssh zip unzip p7zip pigz xorg cmake
+pacman -S --noconfirm base base-devel git vim neovim tmux wget zsh openssh zip unzip p7zip pigz xorg cmake python
 ln -sf /bin/nvim /bin/vi
 
 # locale setting
 sed -i 's:#zh_TW.UTF-8 UTF-8:zh_TW.UTF-8 UTF-8:g' /etc/locale.gen
+sed -i 's:#en_US.UTF-8 UTF-8:en_US.UTF-8 UTF-8:g' /etc/locale.gen
 locale-gen
 echo LANGUAGE=en_US.UTF-8 >> /etc/locale.conf
 echo LC_ALL=en_US.UTF-8 >> /etc/locale.conf
