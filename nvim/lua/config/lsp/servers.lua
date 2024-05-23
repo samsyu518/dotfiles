a4a688a36f8a57dabe184ccb0439566af4a2971c
Sam Syu <samsyu518@gmail.com>
2024-05-23 14:02:43 +0800

    rewrite lsp config

---
+++ nvim/lua/config/lsp/servers.lua
@@ -0,0 +1,44 @@
+return {
+  bashls = true,
+  lua_ls = true,
+  rust_analyzer = true,
+  svelte = true,
+  cssls = true,
+
+  tsserver = true,
+
+  jsonls = {
+    settings = {
+      json = {
+        schemas = require('schemastore').json.schemas(),
+        validate = { enable = true },
+      },
+    },
+  },
+
+  yamlls = {
+    settings = {
+      yaml = {
+        schemaStore = {
+          enable = false,
+          url = '',
+        },
+        schemas = require('schemastore').yaml.schemas(),
+      },
+    },
+  },
+
+  -- lexical = {
+  --   cmd = { vim.fn.expand '$HOME/.local/share/nvim/mason/bin/lexical' },
+  --   root_dir = require('lspconfig.util').root_pattern { 'mix.exs' },
+  -- },
+  nextls = {
+    cmd = { vim.fn.expand '$HOME/.local/share/nvim/mason/bin/nextls', '--stdio' },
+  },
+  elixirls = {
+    cmd = { vim.fn.expand '$HOME/.local/share/nvim/mason/bin/elixir-ls' },
+    root_dir = require('lspconfig.util').root_pattern { 'mix.exs' },
+  },
+
+  pyright = {},
+}
---
+++ nvim/lua/plugins/completion.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    'hrsh7th/nvim-cmp',
+    lazy = false,
+    priority = 100,
+    dependencies = {
+      -- Snippet Engine & its associated nvim-cmp source
+      { 'L3MON4D3/LuaSnip', build = 'make install_jsregexp' },
+      'saadparwaiz1/cmp_luasnip',
+
+      -- Adds LSP completion capabilities
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      -- Adds a number of user-friendly snippets
+      'rafamadriz/friendly-snippets',
+      -- for lsp menu icon
+      'onsails/lspkind.nvim',
+    },
+    config = function()
+      require 'config.completion'
+    end,
+  },
+}
