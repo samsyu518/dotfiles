254ab164a95ffca33cb2ae78b119c0c53ea6e0b0
Sam Syu <samsyu518@gmail.com>
2025-04-18 11:10:51 +0800

    try to switch lazyvim

---
+++ nvim_old/init.lua
@@ -0,0 +1,46 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+
+-- fix cwd to open file path
+local file_arg = vim.v.argv[3]
+if vim.fn.isdirectory(file_arg) == 1 then
+  vim.api.nvim_set_current_dir(file_arg)
+else
+  local file_dir = vim.fn.fnamemodify(file_arg, ":p:h")
+  vim.api.nvim_set_current_dir(file_dir)
+end
+
+-- Install package manager
+--    https://github.com/folke/lazy.nvim
+--    `:help lazy.nvim.txt` for more info
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.uv.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({ import = "plugins" }, {
+  checker = {
+    -- automatically check for plugin updates
+    enabled = false,
+    concurrency = nil, ---@type number? set to 1 to check for updates very slowly
+    notify = true, -- get a notification when new updates are found
+    frequency = 3600, -- check for updates every hour
+    check_pinned = false, -- check for pinned packages that can't be updated
+  },
+  change_detection = { notify = false },
+})
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
