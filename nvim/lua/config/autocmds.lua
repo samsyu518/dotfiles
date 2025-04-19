cf1314d102f42e405db9aed52bedec2884490ede
Sam Syu <samsyu518@gmail.com>
2025-04-19 15:31:26 +0800

    update auto save

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -12,3 +12,31 @@
 --   command = "silent! wall",
 --   nested = true,
 -- })
+local group = vim.api.nvim_create_augroup("autosave", {})
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "AutoSaveEnable",
+  group = group,
+  callback = function(_opts)
+    vim.notify("AutoSave enabled", vim.log.levels.INFO)
+  end,
+})
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "AutoSaveDisable",
+  group = group,
+  callback = function(_opts)
+    vim.notify("AutoSave disabled", vim.log.levels.INFO)
+  end,
+})
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "AutoSaveWritePost",
+  group = group,
+  callback = function(opts)
+    if opts.data.saved_buffer ~= nil then
+      local filename = vim.api.nvim_buf_get_name(opts.data.saved_buffer)
+      vim.notify("AutoSave: saved " .. filename .. " at " .. vim.fn.strftime("%H:%M:%S"), vim.log.levels.INFO)
+    end
+  end,
+})
--- nvim/lua/plugins/autosave.lua
+++ nvim/lua/plugins/autosave.lua
@@ -7,31 +7,18 @@ return {
     -- return false: if it's not ok to be saved
     -- if set to `nil` then no specific condition is applied
     condition = function(buf)
-      local fn = vim.fn
-      local utils = require("auto-save.utils.data")
-
+      local filetype = vim.fn.getbufvar(buf, "&filetype")
       -- don't save for `sql` file types
-      if utils.not_in(fn.getbufvar(buf, "&filetype"), { "sql", "mysql", "dbui", "dbout", "plsql" }) then
-        return true
+      if vim.list_contains({ "sql", "mysql", "dbui", "dbout", "plsql", "OverseerForm" }, filetype) then
+        return false
+        -- don't save for special-buffers
+      elseif vim.fn.getbufvar(buf, "&buftype") ~= "" then
+        return false
       end
-      return false
+      return true
     end,
     debounce_delay = 2000, -- delay after which a pending save is executed
     -- log debug messages to 'auto-save.log' file in neovim cache directory, set to `true` to enable
     debug = false,
   },
-  config = function(_, opts)
-    require("auto-save").setup(opts)
-    vim.api.nvim_set_keymap("n", "<leader>fa", ":ASToggle<CR>", {})
-    vim.api.nvim_create_autocmd("User", {
-      pattern = "AutoSaveWritePost",
-      group = group,
-      callback = function(opts)
-        if opts.data.saved_buffer ~= nil then
-          local filename = vim.api.nvim_buf_get_name(opts.data.saved_buffer)
-          print("AutoSave: saved " .. filename .. " at " .. vim.fn.strftime("%H:%M:%S"))
-        end
-      end,
-    })
-  end,
 }
