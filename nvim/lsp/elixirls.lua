81cc9529f2de705608dfd0b9e79140a2b1647652
Sam Syu <samsyu518@gmail.com>
2025-11-27 17:44:38 +0800

    fix elixirls stdlibsrc for go to source code

--- nvim/lsp/elixirls.lua
+++ nvim/lsp/elixirls.lua
@@ -2,9 +2,12 @@ return {
   flags = {
     debounce_text_changes = 150,
   },
-  elixirLS = {
-    -- dialyzerEnabled = false,
-    -- fetchDeps = false,
-    stdlibSrcDir = "$HOME/.local/share/mise/installs/elixir/1.16.3-otp-26",
+  settings = {
+    elixirLS = {
+      dialyzerEnabled = true,
+      -- fetchDeps = false,
+      -- .local/share/mise/installs/elixir/1.16.3-otp-26
+      stdlibSrcDir = vim.fn.expand("$HOME/.local/share/mise/installs/elixir/1.16.3-otp-26"),
+    },
   },
 }
