06fda82706679373f56d58ada63e28acf097d168
Sam Syu <samsyu518@gmail.com>
2026-05-24 10:11:35 +0800

    update init

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,7 +14,6 @@ end
 ---@type vim.Option
 local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
-
 -- [[ Configure and install plugins ]]
 --
 --  To check the current status of your plugins, run
@@ -29,34 +28,13 @@ require("lazy").setup({
 
   { import = "plugins.ai" },
   { import = "plugins.autosave" },
+  { import = "plugins.coding" },
   { import = "plugins.colorscheme" },
   { import = "plugins.editor" },
   { import = "plugins.lsp" },
   { import = "plugins.treesitter" },
   { import = "plugins.ui" },
   { import = "plugins.utils" },
-  { -- Collection of various small independent plugins/modules
-    "echasnovski/mini.nvim",
-    config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-      --  - ci'  - [C]hange [I]nside [']quote
-      require("mini.ai").setup({ n_lines = 500 })
-
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require("mini.surround").setup()
-
-      -- ... and there is more!
-      --  Check out: https://github.com/echasnovski/mini.nvim
-    end,
-  },
   {
 
     -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
@@ -74,12 +52,6 @@ require("lazy").setup({
     -- require 'kickstart.plugins.autopairs',
     require("kickstart.plugins.neo-tree"),
     -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
-
-    --
-    -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
-    -- Or use telescope!
-    -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
-    -- you can continue same window with `<space>sr` which resumes last telescope search
   },
 }, {
   ui = {
