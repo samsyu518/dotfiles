d812968a4e1cb9afbdbec84011cd091efd7abb53
Sam Syu <samsyu518@gmail.com>
2026-05-28 23:02:38 +0800

    fix snake cwd

--- nvim/lua/plugins/utils/snacks.lua
+++ nvim/lua/plugins/utils/snacks.lua
@@ -41,9 +41,11 @@ return {
       -- { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
       { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
     { "<leader>ff", function()
-      Snacks.picker.files({ cmd ="rg"})
+      Snacks.picker.files({ cmd = "rg", cwd = vim.fn.getcwd() })
     end, desc = "Find Files" },
-      { "<leader>fg", function() Snacks.picker.grep() end, desc = "Grep Files" },
+    { "<leader>fg", function()
+      Snacks.picker.grep({ cwd = vim.fn.getcwd() })
+    end, desc = "Grep Files" },
       { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
       { "<leader>fr", function() Snacks.picker.resume() end, desc = "Resume" },
       -- git
